ffbad45c536db42419089e8d1ab3b1991c67cac8
jiry1994 <jiry1994@live.nl>
2015-03-24 15:22:40 +0100

    Survey.JAVA

--- app/src/main/java/nl/hu/team/ntrapplication/objects/Survey.java
+++ app/src/main/java/nl/hu/team/ntrapplication/objects/Survey.java
@@ -1,7 +1,74 @@
 package nl.hu.team.ntrapplication.objects;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Milamber on 24-3-2015.
  */
 public class Survey {
+    private String name;
+    private Date beginDate;
+    private Date endDate;
+    private String status;
+
+    // Constructor
+    public Survey() {
+
+    }
+    // Variabelen
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private Date convertString(String input) {
+        DateFormat format = new SimpleDateFormat("dd-mm-yyyy");
+        Date date = null;
+        try {
+            date = format.parse(input);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
 }
