3647e98e215fc7633c8b30c29770debf4ccb732d
Wartijn <martijnvansloten@gmail.com>
2015-03-24 16:47:15 +0100

    Een aantal dingen veranderd

--- app/src/main/java/nl/hu/team/ntrapplication/TijdelijkeClaas.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.team.ntrapplication;
-
-/**
- * Created by Tinus on 24-3-2015.
- */
-public class TijdelijkeClaas {
-}
--- app/src/main/java/nl/hu/team/ntrapplication/activities/MainActivity.java
+++ app/src/main/java/nl/hu/team/ntrapplication/activities/MainActivity.java
@@ -1,17 +1,20 @@
-package nl.hu.team.ntrapplication;
+package nl.hu.team.ntrapplication.activities;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import nl.hu.team.ntrapplication.R;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.video_fragment);
     }
 
 
