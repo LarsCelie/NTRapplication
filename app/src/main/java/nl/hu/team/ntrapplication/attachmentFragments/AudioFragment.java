c3b1b533476e37ec1e53a62931711c916d35cb09
Wartijn <martijnvansloten@gmail.com>
2015-04-01 13:27:47 +0200

    packages toegevoegd

--- app/src/main/java/nl/hu/team/ntrapplication/attachmentFragments/AudioFragment.java
+++ app/src/main/java/nl/hu/team/ntrapplication/attachmentFragments/AudioFragment.java
@@ -1,10 +1,35 @@
 package nl.hu.team.ntrapplication.attachmentFragments;
 
 import android.app.Fragment;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import nl.hu.team.ntrapplication.R;
 
 /**
  * Created by Tinus on 1-4-2015.
  */
 public class AudioFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Uri uri = Uri.parse("android.resource://" + getActivity().getPackageName() + "/" + R.raw.audio_test_01);
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(getApplicationContext(), myUri);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
+        return null;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+    }
+
 
 }
