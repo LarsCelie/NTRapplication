8142e468bbbce52c85b42cead0103f774777d5e9
Milamber <ff-denke@hotmail.com>
2015-04-01 10:57:07 +0200

    Menu change research list

--- app/src/main/java/nl/hu/team/ntrapplication/activities/VideoFragment.java
+++ app/src/main/java/nl/hu/team/ntrapplication/activities/VideoFragment.java
@@ -3,12 +3,10 @@ package nl.hu.team.ntrapplication.activities;
 import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -25,7 +23,7 @@ public class VideoFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.video_fragment, container, false);
+        View root = inflater.inflate(R.layout.fragment_video, container, false);
         videoView = (VideoView) root.findViewById(R.id.video_view);
 
         return root;
