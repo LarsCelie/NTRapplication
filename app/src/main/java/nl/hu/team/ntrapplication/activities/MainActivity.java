7115138495ff07c921f631643215a8e404129e57
Wartijn <martijnvansloten@gmail.com>
2015-04-02 10:58:33 +0200

    Paar kleine dingetjes

--- app/src/main/java/nl/hu/team/ntrapplication/activities/MainActivity.java
+++ app/src/main/java/nl/hu/team/ntrapplication/activities/MainActivity.java
@@ -15,6 +15,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import nl.hu.team.ntrapplication.R;
+import nl.hu.team.ntrapplication.attachmentFragments.AudioFragment;
 import nl.hu.team.ntrapplication.attachmentFragments.VideoFragment;
 import nl.hu.team.ntrapplication.database.DatabaseHandler;
 import nl.hu.team.ntrapplication.objects.Attachment;
@@ -126,7 +127,7 @@ public class MainActivity extends Activity {
     }
 
     public void openFragment(View view){
-        Fragment newFragment = new VideoFragment();
+        Fragment newFragment = new AudioFragment();
        //De volgende regel is waarschijnlijk fout.
         //((VideoFragment)newFragment).playVideo();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
