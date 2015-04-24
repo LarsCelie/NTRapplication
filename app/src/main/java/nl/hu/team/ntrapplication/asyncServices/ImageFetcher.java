33d7f78c325952d31c6e87dc99d832acacba2009
Milamber <ff-denke@hotmail.com>
2015-04-24 13:57:59 +0200

    image from rest service

---
+++ app/src/main/java/nl/hu/team/ntrapplication/asyncServices/ImageFetcher.java
@@ -0,0 +1,28 @@
+package nl.hu.team.ntrapplication.asyncServices;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ImageFetcher extends AsyncTask<URL,String,String> {
+
+    protected String doInBackground(URL... url) {
+        try {
+            HttpURLConnection urlConnection = (HttpURLConnection) url[0].openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            InputStreamReader is = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(is);
+            String result = br.readLine();
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
--- app/src/main/java/nl/hu/team/ntrapplication/attachmentFragments/ImageFragment.java
+++ app/src/main/java/nl/hu/team/ntrapplication/attachmentFragments/ImageFragment.java
@@ -1,14 +1,36 @@
 package nl.hu.team.ntrapplication.attachmentFragments;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import nl.hu.team.ntrapplication.R;
 import nl.hu.team.ntrapplication.objects.Attachment;
+import nl.hu.team.ntrapplication.asyncServices.*;
 
 /**
  * Created by Lars on 4/1/2015.
@@ -33,26 +55,20 @@ public class ImageFragment extends Fragment {
         image = (ImageView) getView().findViewById(R.id.image_view);
         //check if attachment is an image
         if (attachment.getTYPE().equals("image")){
-                int resID = getResources().getIdentifier(attachment.getLOCATION(), "drawable", getActivity().getPackageName());
-                image.setImageResource(resID);
+            int resID = getResources().getIdentifier(attachment.getLOCATION(), "drawable", getActivity().getPackageName());
+            image.setImageResource(resID);
+            //used for REST call of image objects ip 10.0.2.2 for localhost in VM
+//            try {
+//                URL url = new URL("http://10.0.2.2:8080/NTR_application/rest/image/fruit");
+//                String result=new ImageFetcher().execute(url).get();
+//                byte[] decodedString = Base64.decode(result, Base64.DEFAULT);
+//                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+//                image.setImageBitmap(decodedByte);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
         }
     }
-//    public static Bitmap getBitmapFromURL(String src) {
-//        try {
-//
-//            URL url = new URL(src);
-//            HttpURLConnection connection = (HttpURLConnection) url
-//                    .openConnection();
-//            connection.setDoInput(true);
-//            connection.connect();
-//            InputStream input = connection.getInputStream();
-//            Bitmap mybitmap = BitmapFactory.decodeStream(input);
-//
-//            return mybitmap;
-//
-//        } catch (Exception ex) {
-//            System.out.println("null image bitmap");
-//            return null;
-//        }
-//    }
+
+
 }
