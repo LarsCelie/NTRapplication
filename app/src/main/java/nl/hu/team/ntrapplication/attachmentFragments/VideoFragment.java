7159c01f5cb83ccf5db8511e56df61a6f2f27da1
Milamber <ff-denke@hotmail.com>
2015-06-08 14:26:21 +0200

    using Rest service to stream audio and video now dynamic, requires filename for location of attachment

--- app/src/main/java/nl/hu/team/ntrapplication/attachmentFragments/VideoFragment.java
+++ app/src/main/java/nl/hu/team/ntrapplication/attachmentFragments/VideoFragment.java
@@ -8,9 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.VideoView;
 
 import nl.hu.team.ntrapplication.R;
+import nl.hu.team.ntrapplication.objects.Attachment;
 
 /**
  * Created by Milamber on 29-5-2015.
@@ -20,17 +20,25 @@ public class VideoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_video, container, false);
-        final Button button = (Button) root.findViewById(R.id.video);
+        return root;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Bundle bundle = this.getArguments();
+        //get the attachment
+        final Attachment attachment = bundle.getParcelable("attachment");
+        final Button button = (Button) getView().findViewById(R.id.video);
         button.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setDataAndType(Uri.parse("http://10.0.2.2:8080/NTR_application/rest/watch"), "video/mp4");
+                intent.setDataAndType(Uri.parse("http://10.0.2.2:8080/NTR_application/rest/watch"+attachment.getLOCATION()), "video/*");
                 startActivity(intent);
-
             }
 
         });
-        return root;
     }
 }
